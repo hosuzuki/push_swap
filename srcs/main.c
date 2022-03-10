feacfd9f2806725b91c07ce66d08922f41cbbc03
Hokuto Suzuki <81731043+HawkSUZUKI@users.noreply.github.com>
2022-03-10 17:42:17 +0900

    modified

--- srcs/main.c
+++ srcs/main.c
@@ -15,11 +15,6 @@ int main(int argc, char **argv)
 		return (0);
 	}
 	stack_a = init_stack(args, argv);
-	stack_b = init_stack(0, NULL);
-	if (!stack_a || !stack_b)
-		shutdown(STACK_ERROR);
-	sort = pre_sort(stack_a);
-	record = record_array(stack_a->val * 12);
 
 	return (0);
 }
