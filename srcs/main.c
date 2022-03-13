b69b9e884cc34054b7afda4c0e5b7b7cf4c7b5f8
Hokuto Suzuki <81731043+HawkSUZUKI@users.noreply.github.com>
2022-03-13 16:00:57 +0900

    updates on printing check

--- srcs/main.c
+++ srcs/main.c
@@ -40,40 +40,26 @@ int main(int argc, char **argv) // hve to be much simpler/ get argv /sort/print 
 {
 	t_stack	*stack_a;
 	t_stack	*stack_b;
-	//t_sort	*sort;
+	t_sort	*sort;
 	//char	**record;
 	
 	if (argc < 2)
 		return (0);
 	stack_a = validate_number(argc - 1, argv + 1);
 	stack_b = init_stack(0, NULL);
-	for (int i = 0; i < 5 ; i++)
+	
+	t_stack	*tmp = stack_a;
+	for (int i = 0; i < argc - 1 ; i++)
 	{
-		stack_a = stack_a->next;
-		printf("stack_a: %d\n", stack_a->val);
+		tmp = tmp->next;
+		printf("[%d]: %d\n", i + 1, tmp->val);
 	}
-	free_stack(stack_a, 5);
+	free_stack(stack_a, argc - 1);
 	free_stack(stack_b, 0);
-/*
-	if (!is_digit_str(argc - 1, argv + 1))
-	{
-		write(2, "Error\n", 6);
-		return (0);
-	}
-	stack_a = init_stack(argc, argv);
-	stack_b = init_stack(0, NULL);
-
-	if (!stack_a || !stack_b) // separate
-		shutdown(MAIN_STACK_ERROR);
-*/
-
-
-
-	/*
 	sort = pre_sort(stack_a);
-	record = record_array(stack_a->val * 12);
-//	if (!is_unique(stack_a)) // this have to be before sort
-//		error_exit();
+
+
+/*	record = record_array(stack_a->val * 12);
 	if (!sort || !record || is_sorted(stack_a))
 		shutdown(MAIN_SORT_ERROR);
 	size_branch(stack_a, stack_b, sort);
