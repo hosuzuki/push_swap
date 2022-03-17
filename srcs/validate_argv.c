592f273db58098bdb4fe27acfce7b12cfb1f2bf5
Hokuto Suzuki <81731043+HawkSUZUKI@users.noreply.github.com>
2022-03-17 14:17:26 +0900

    final draft is done

--- srcs/validate_argv.c
+++ srcs/validate_argv.c
@@ -6,7 +6,7 @@
 /*   By: hokutosuzuki <hosuzuki@student.42toky      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/16 21:26:53 by hokutosuz         #+#    #+#             */
-/*   Updated: 2022/03/16 21:26:53 by hokutosuz        ###   ########.fr       */
+/*   Updated: 2022/03/17 14:00:00 by hokutosuz        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,12 @@ void	scan_dupulicates(t_stack *a, t_storage *storage)
 	}
 }
 
+static void	print_error_and_exit(enum e_error_handling status)
+{
+	write(1, "Error\n", 6);
+	exit (status);
+}
+
 void	validate_argv(int argc, char **argv)
 {
 	int		i;
@@ -45,16 +51,13 @@ void	validate_argv(int argc, char **argv)
 		i = 0;
 		str = argv[j];
 		if (str[0] == '\0')
-			exit (VALIDATE_ARGV1);
+			print_error_and_exit(VALIDATE_ARGV1);
 		if (str[0] == '-' && str[1] != '\0')
 			i++;
 		while (str[i])
 		{
 			if (!ft_isdigit(str[i]))
-			{
-				write(1, "Error\n", 6);
-				exit (VALIDATE_ARGV2);
-			}
+				print_error_and_exit(VALIDATE_ARGV2);
 			i++;
 		}
 		j++;
---
+++ valg_checker.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "1: alphabet"
+ARG="1 3 one 9 5"; valgrind ./checker $ARG 
+echo "2: mark"
+ARG="1 3 5 7 9 *"; valgrind ./checker $ARG 
+ARG="1 3 5 7 9 -"; valgrind ./checker $ARG 
+echo "3: over INT_MAX"
+ARG="1 3 9 5 80 38 277 2147483648"; valgrind ./checker $ARG 
+echo "4: over INT_MIN"
+ARG="1 3 -20 209 9 5 -2147483649"; valgrind ./checker $ARG 
+echo "5: duplicates"
+ARG="1 3 9 5 0 1"; valgrind ./checker $ARG 
+ARG="1 3 9 5 0 480 288 17 3990 30399 84 3"; valgrind ./checker $ARG 
+echo "6: \"\""
+ARG="1 3 9 5 0 \"\" 8"; valgrind ./checker $ARG 
